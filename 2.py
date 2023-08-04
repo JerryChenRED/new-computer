afdb51ce8e1e8663939bdf3230baba26be0ddeb1
JerryChenRED <jerry.chen0127@gmail.com>
2023-08-04 11:59:58 +0800

    standard_deviation

--- 2.py
+++ 2.py
@@ -1,1 +1,18 @@
-print(123)
+import math
+
+numCount = int(input("有幾個數字: "))
+numbers = []
+
+print(f"請輸入{numCount}個數字:")
+for i in range(numCount):
+    inputNum = float(input())
+    numbers.append(inputNum)
+
+average = sum(numbers) / len(numbers)
+
+variance = sum((num - average) ** 2 for num in numbers) / len(numbers)
+standard_deviation = math.sqrt(variance)
+
+print(f"平均值: {average}")
+print(f"標準差: {standard_deviation}")
+
